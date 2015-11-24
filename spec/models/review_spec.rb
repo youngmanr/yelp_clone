da27f9668e9a2d743e47197a607a65c5a6047a24
Norm Fasey <norm@fasey.net>
2015-11-24 16:46:18 +0000

    validations

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -2,4 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Review, type: :model do
   it {should belong_to(:restaurant)}
+  it "is invalid if the rating is more than 5" do
+    review = Review.new(rating: 10)
+    expect(review).to have(1).error_on(:rating)
+  end
 end
