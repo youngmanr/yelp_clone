b75dab068c9dddf7ef5a281bb082cba78e9055ed
Yev Dyko <evgeny.dyko@gmail.com>
2015-11-25 17:24:56 +0300

    Added OmniAuth

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :restaurants do
     resources :reviews
   end
+
   root to: "restaurants#index"
 end
