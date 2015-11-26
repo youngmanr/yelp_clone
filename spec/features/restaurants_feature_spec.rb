bf5e2924b99eb6827718a1a449835128568c30a2
blancopado <pablo.dominguezblanco@gmail.com>
2015-11-26 17:08:47 +0100

    Revert "setting limit on user must be logged in to create restaurant"
    This reverts commit 2d6ca5a9380a6ffc32a7864f20c17399303bd4fc.

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
-require_relative '../helpers/users_helper_spec'
 
 feature 'restaurants' do
   context 'no restaurants have been added' do
@@ -21,10 +20,8 @@ feature 'restaurants' do
       expect(page).not_to have_content('No restaurants yet')
     end
   end
-
   context 'creating restaurants' do
     scenario 'prompts user to fill out a form, then displays the new restaurant' do
-      sign_up
       visit '/restaurants'
       click_link 'Add a restaurant'
       fill_in 'Name', with: 'KFC'
@@ -34,9 +31,7 @@ feature 'restaurants' do
     end
     context 'an invalid restaurant' do
       it 'does not let you submit a name that is too short' do
-        sign_up
-        #visit('/')
-        #visit '/restaurants'
+        visit '/restaurants'
         click_link 'Add a restaurant'
         fill_in 'Name', with: 'kf'
         click_button 'Create Restaurant'
@@ -59,7 +54,7 @@ feature 'restaurants' do
     before { Restaurant.create name: 'KFC' }
 
     scenario 'let a user edit a restaurant' do
-      sign_up
+      visit '/restaurants'
       click_link 'Edit KFC'
       fill_in 'Name', with: 'Kentucky Fried Chicken'
       click_button 'Update Restaurant'
@@ -72,7 +67,7 @@ feature 'restaurants' do
     before {Restaurant.create name: 'KFC'}
 
     scenario 'removes a restaurant when a user clicks a delete link' do
-      sign_up
+      visit '/restaurants'
       click_link 'Delete KFC'
       expect(page).not_to have_content 'KFC'
       expect(page).to have_content 'Restaurant deleted successfully'
