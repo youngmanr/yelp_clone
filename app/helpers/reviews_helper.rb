574a395c912ad65542ecf76021677cb1ef208dc5
Robert Youngman <rob_youngman@yahoo.com>
2015-11-27 10:33:48 +0000

    three black stars test

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,6 +1,7 @@
 module ReviewsHelper
 
 	def star_rating rating
-		rating
+    return rating unless rating.is_a?(Fixnum)
+		'★' * rating
 	end
 end
--- spec/helpers/reviews_helper_spec.rb
+++ spec/helpers/reviews_helper_spec.rb
@@ -18,5 +18,8 @@ RSpec.describe ReviewsHelper, type: :helper do
     it 'returns five black stars for five' do
     	expect(helper.star_rating(5)).to eq '★★★★★'
     end
+    it 'return three black stars and two white stars for three' do
+      expect(helper.star_rating(3)).to eq '★★★☆☆'
+    end
   end
 end
